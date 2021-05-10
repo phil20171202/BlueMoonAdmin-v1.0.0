ac6a089386078d62d7fba226d92c2ad620dc9642
David Adamson <david@212digital.co.uk>
2021-05-10 10:19:52 +0100

    added the create customer MVC

--- Models/Customers.cs
+++ Models/Customers.cs
@@ -1,8 +1,36 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace BlueMoonAdmin.Models
 {
     public class Customers
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Comapny Name")]
+        public string CompanyName { get; set; }
+
+        [Display(Name = "Contact Name")]
+        public string ContactName { get; set; }
+
+        [Display(Name = "Office Address")]
+        public string OfficeAddress { get; set; }
+
+        [Phone]
+        [Display(Name = "Telephone Number")]
+        public string TelephoneNumber { get; set; }
+
+        [Display(Name = "Service Contract")]
+        public string ServiceContract { get; set; }
+
+        [Display(Name = "Last Service Date")]
+        public DateTime LastServiceDate { get; set; }
+
+        [Display(Name = "Current Machine")]
+        public string CurrentMachine { get; set; }
+
+        [Display(Name = "Customer Notes")]
+        public string CustomerNotes { get; set; }
     }
 }
