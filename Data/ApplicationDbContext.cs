5830902f2dd6bec530d11dc2550b559fe5e0bf1f
David Adamson <david@212digital.co.uk>
2021-05-10 08:26:08 +0100

    added to gitignore file

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BlueMoonAdmin.Models;
+
+namespace BlueMoonAdmin.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ServiceCustomer> ServiceCustomers { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
