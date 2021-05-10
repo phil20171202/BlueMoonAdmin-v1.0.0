6f7ca1152a4fecb011cd6ee8c173f67e6121ecb4
phil20171202 <34198660+phil20171202@users.noreply.github.com>
2021-05-10 19:11:24 +0100

    Working Customer Index,Create,Edit pages - Layout not correct

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlueMoonAdmin.Data;
+using BlueMoonAdmin.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,20 +10,67 @@ namespace BlueMoonAdmin.Controllers
 {
     public class CustomersController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CustomersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+
         public IActionResult Index()
         {
-            return View();
+
+            IEnumerable<Customers> objList = _db.Customers;
+            return View(objList);
         }
 
         public IActionResult CreateCustomer()
         {
+
             return View();
         }
 
-        public IActionResult ViewCustomer()
+        // POST-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateCustomer(Customers obj)
         {
-            return View();
+            _db.Customers.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
+
+        // GET-Update
+        public IActionResult UpdateCustomer(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Customers.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        // POST-Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateCustomer(Customers obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Customers.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
     }
 
 }
