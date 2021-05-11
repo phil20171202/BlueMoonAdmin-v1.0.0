8bbdb289fe04a88d9310eb1b616ba6e8106d2771
David Adamson <david@212digital.co.uk>
2021-05-11 16:15:42 +0100

    added to do list View/Controller

--- Controllers/GoogleMapsController.cs
+++ Controllers/GoogleMapsController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BlueMoonAdmin.Controllers
 {
-    public class GoogleMaps : Controller
+    public class GoogleMapsController : Controller
     {
         public IActionResult Index()
         {
