20d28b73cac6587fee0ccbe554fafc196f68c64b
phil20171202 <34198660+phil20171202@users.noreply.github.com>
2021-05-13 21:23:37 +0100

    Customer View model
    View model and working Customer view, shows contacts + customer name shows in header

---
+++ Models/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueMoonAdmin.Models.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public Customers Customers { get; set; }
+        public IEnumerable<Contacts> Contacts { get; set; }
+        
+    }
+}
