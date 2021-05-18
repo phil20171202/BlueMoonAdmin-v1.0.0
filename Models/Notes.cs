d1b3eb78c16eb661ce161809acd2f174e00fabcb
phil20171202 <34198660+phil20171202@users.noreply.github.com>
2021-05-18 13:52:41 +0100

    Added Note model/class

---
+++ Models/Notes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueMoonAdmin.Models
+{
+    public class Notes
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string notes { get; set; }
+
+        [DataType(DataType.Date)]
+        public Nullable<DateTime> Date { get; set; }
+
+        public int CustomerId { get; set; }
+        [ForeignKey("CustomerId")]
+        public virtual Customers Customer { get; set; }
+
+        // might want to link to Signed on user, so you know who added the note
+    }
+}
