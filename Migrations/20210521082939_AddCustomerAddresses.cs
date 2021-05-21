2d5a25ca017d908b49faec73015a7c2fc6bd15e6
David Adamson <david@212digital.co.uk>
2021-05-21 10:05:19 +0100

    create addres migration and removed chart.js files from views that didn't need them

---
+++ Migrations/20210521082939_AddCustomerAddresses.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlueMoonAdmin.Migrations
+{
+    public partial class AddCustomerAddresses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
