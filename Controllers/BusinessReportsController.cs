301aa66c7d19ef04e8cd6b6c1c10083cba080d7e
David Adamson <david@212digital.co.uk>
2021-05-25 10:58:21 +0100

    added new reports dashboards for discussion

---
+++ Controllers/BusinessReportsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueMoonAdmin.Controllers
+{
+    public class BusinessReportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BusinessSnapshot()
+        {
+            return View();
+        }
+    }
+}
