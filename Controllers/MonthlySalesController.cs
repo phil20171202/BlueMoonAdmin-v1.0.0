b09d49b671b4ed2f00b828cdcdbc433a866cdbc3
David Adamson <david@212digital.co.uk>
2021-05-25 16:09:32 +0100

    sales figures model and migration done

--- Controllers/MonthlySalesController.cs
+++ Controllers/MonthlySalesController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlueMoonAdmin.Data;
+using BlueMoonAdmin.Models;
+using BlueMoonAdmin.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +12,34 @@ namespace BlueMoonAdmin.Controllers
 {
     public class MonthlySalesController : Controller
     {
-        public IActionResult MonthlySales()
+        
+        private readonly ApplicationDbContext _db;
+    
+        public MonthlySalesController (ApplicationDbContext db)
         {
+            _db = db;
+        }
+         public IActionResult MonthlySales()
+        {
+            IEnumerable<MonthlySales> objList = _db.MonthlySalesFigure;
+            return View(objList);
+        }
+
+        public IActionResult Create()
+        {
+
             return View();
         }
+
+        // POST-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(MonthlySales obj)
+        {
+            _db.MonthlySalesFigure.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("MonthlySales");
+        }
+
     }
 }
