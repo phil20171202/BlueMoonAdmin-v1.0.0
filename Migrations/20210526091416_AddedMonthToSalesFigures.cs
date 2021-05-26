df0eefb313a24e191f71eaf19dcde9426a625a30
David Adamson <david@212digital.co.uk>
2021-05-26 10:14:52 +0100

    added month field to sales figures table

---
+++ Migrations/20210526091416_AddedMonthToSalesFigures.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlueMoonAdmin.Migrations
+{
+    public partial class AddedMonthToSalesFigures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Month",
+                table: "MonthlySalesFigure",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Month",
+                table: "MonthlySalesFigure");
+        }
+    }
+}
