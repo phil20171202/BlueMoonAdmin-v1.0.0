4b2aaa2ef95db9c3b6327bdfd645057037b33954
David Adamson <david@212digital.co.uk>
2021-05-26 11:10:22 +0100

    added current and previous year bar/line chart to monthly sales dashboard

--- wwwroot/assets/demo/chart-bar-customer.js
+++ wwwroot/assets/demo/chart-bar-customer.js
@@ -5,16 +5,28 @@ Chart.defaults.global.defaultFontColor = '#292b2c';
 // Bar Chart Example
 var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-      labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-    datasets: [{
-      label: "Revenue",
-      backgroundColor: "rgba(2,117,216,1)",
-      borderColor: "rgba(2,117,216,1)",
-      data: [82156, 52312, 66251, 77841, 59821, 14984, 0,0,0,0,0,0],
-    }],
-  },
+
+    type: 'bar',
+    data: {
+        datasets: [{
+            label: 'Current Year',
+            backgroundColor: "rgba(2,117,216,1)",
+            borderColor: "rgba(2,117,216,1)",
+            data: [82156, 52312, 66251, 77841, 59821, 14984, 0, 0, 0, 0, 0, 0],
+            // this dataset is drawn below
+            order: 2
+        }, {
+            label: 'Previous Year',
+            backgroundColor: "rgba(255,255,255)",
+            borderColor: "rgba(23,186,239)",
+            data: [62156, 42312, 56251, 87841, 49821, 24984, 0, 0, 0, 0, 0, 0],
+            type: 'line',
+            // this dataset is drawn on top
+            order: 1
+        }],
+        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    },
+
   options: {
     scales: {
       xAxes: [{
@@ -40,7 +52,7 @@ var myLineChart = new Chart(ctx, {
       }],
     },
     legend: {
-      display: false
+      display: true
     }
   }
 });
