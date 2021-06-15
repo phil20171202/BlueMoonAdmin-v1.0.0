aa0715dff1f1911f1283f1fb15ba8097540e9627
phil20171202 <34198660+phil20171202@users.noreply.github.com>
2021-06-15 11:05:27 +0100

    Changed home dashboard to view model. Added Task (needs formating) Gave example of customer count.

---
+++ Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BlueMoonAdmin.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueMoonAdmin.Models.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public IEnumerable<ToDoListItem> toDoList { get; set; }
+
+        public int CustomerCount { get; set; }
+
+        public int ServiceCount { get; set; }
+
+        public Customers Customers { get; set; }
+
+    }
+}
