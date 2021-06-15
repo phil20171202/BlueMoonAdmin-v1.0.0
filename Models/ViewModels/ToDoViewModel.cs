5cdb99eb64611cf6aec97b16a610803827ea763e
phil20171202 <34198660+phil20171202@users.noreply.github.com>
2021-06-15 13:50:16 +0100

    Task counts added to TodoList and main dashboard

---
+++ Models/ViewModels/ToDoViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueMoonAdmin.Models.ViewModels
+{
+    public class ToDoViewModel
+    {
+
+    public IEnumerable<ToDoListItem> Tasks { get; set; }
+
+    public int TaskCompletedCount { get; set; }
+
+    public int TaskCompleteCount { get; set; }
+    public int TaskOverdueCount { get; set; }
+
+    public int TaskPendingCount { get; set; }
+
+    }
+
+}
