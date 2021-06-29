47f59a820a4d178750bd72811c67d23f742ea646
David Adamson <david@212digital.co.uk>
2021-06-29 11:08:08 +0100

    NuGet Package installed - Dapper for chartjs

--- wwwroot/assets/demo/chart-bar-monthly.js
+++ wwwroot/assets/demo/chart-bar-monthly.js
@@ -4,56 +4,71 @@ Chart.defaults.global.defaultFontColor = '#292b2c';
 
 // Bar Chart Example
 var ctx = document.getElementById("barChartMonthly");
-var myLineChart = new Chart(ctx, {
-
-    type: 'bar',
-    data: {
-        datasets: [{
-            label: 'Current Year',
-            backgroundColor: "rgba(2,117,216, 0.7)",
-            borderColor: "rgba(2,117,216,1)",           
-            data: [82156, 52312, 66251, 77841, 59821, 14984, 0, 0, 0, 0, 0, 0],
-            // this dataset is drawn below
-            opacity: 80,
-            order: 2
-        }, {
-            label: 'Previous Year',
-            backgroundColor: "rgba(255,255,255, 0.1)",
-            borderColor: "rgba(23,186,239)",
-            data: [62156, 42312, 56251, 87841, 49821, 24984, 0, 0, 0, 0, 0, 0],
-            type: 'line',
-            // this dataset is drawn on top
-            order: 1
-        }],
-        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    },
-
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
-        },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 12
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 100000,
-          maxTicksLimit: 6
-        },
-        gridLines: {
-          display: true
-        }
-      }],
-    },
-    legend: {
-      display: true
+$(function () {
+    var labels = '<%=TempData["ChartSales"] %>';
+    var datas = '<%=TempData["ChartSales"] %>';
+    var labelsArray = new Array();
+    var dataArray = new Array();
+    for (var i = 0; i < labels.split(',').length; i++) {
+        labelsArray.push(labels.split(',')[i]);
+        dataArray.push(datas.split(',')[i]);
     }
-  }
-});
+    var myLineChart = new Chart(ctx, {
+
+        type: 'bar',
+        data: {
+            datasets: [{
+                label: 'Current Year',
+                backgroundColor: "rgba(2,117,216, 0.7)",
+                borderColor: "rgba(2,117,216,1)",
+                data: {
+                    labels: labelsArray,
+                    datasets: [{
+                        label: '% Current Year',                       
+                        data: dataArray
+                    }]
+                },
+                opacity: 80,
+                order: 2
+            }, {
+                label: 'Previous Year',
+                backgroundColor: "rgba(255,255,255, 0.1)",
+                borderColor: "rgba(23,186,239)",
+                data: [62156, 42312, 56251, 87841, 49821, 24984, 0, 0, 0, 0, 0, 0],
+                type: 'line',
+                // this dataset is drawn on top
+                order: 1
+            }],
+            labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        },
+
+        options: {
+            scales: {
+                xAxes: [{
+                    time: {
+                        unit: 'month'
+                    },
+                    gridLines: {
+                        display: false
+                    },
+                    ticks: {
+                        maxTicksLimit: 12
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        min: 0,
+                        max: 100000,
+                        maxTicksLimit: 6
+                    },
+                    gridLines: {
+                        display: true
+                    }
+                }],
+            },
+            legend: {
+                display: true
+            }
+        }
+    });
+})
